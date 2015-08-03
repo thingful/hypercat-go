2d5357554032a4ef3d887da8d447c15414340622
Sam Mulube <sam@umbrellium.co.uk>
2015-08-03 22:18:49 +0100

    Change all comment styles to adhere to go lint suggestions

--- metadata.go
+++ metadata.go
@@ -1,23 +1,17 @@
 package hypercat
 
-/*
- * Rel is the representation of the HyperCat 2.0 individual metadata object
- * which is used to describe a relationship between an entity and some other
- * entity or concept.
- */
+// Rel is the representation of the HyperCat 2.0 individual metadata object
+// which is used to describe a relationship between an entity and some other
+// entity or concept.
 type Rel struct {
 	Rel string `json:"rel"`
 	Val string `json:"val"`
 }
 
-/*
- * Metadata is a simple type alias for a slice of Rel structs.
- */
+// Metadata is a simple type alias for a slice of Rel structs.
 type Metadata []Rel
 
-/*
- * NewRel is a constructor function that creates and returns a Rel instance.
- */
+// NewRel is a constructor function that creates and returns a Rel instance.
 func NewRel(rel, val string) *Rel {
 	return &Rel{
 		Rel: rel,
